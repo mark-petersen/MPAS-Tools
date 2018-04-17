53251d1d7781d38b5540aef39ca4620d237d1cce
Mark Petersen <mpetersen@lanl.gov>
2018-04-17 13:04:59 -0600

    Working so far

--- grid_gen/jigsaw_to_MPAS/build_mesh.sh
+++ grid_gen/jigsaw_to_MPAS/build_mesh.sh
@@ -14,9 +14,10 @@ JIGSAW='/usr/projects/climate/mpeterse/repos/jigsaw-geo-matlab/master'
 MATLAB='octave --silent --eval '
 
 # fill in longer paths, based on paths above:
-JIGSAW2NETCDF=$MPASTOOLS/grid_gen/${MESHNAME}_triangle_jigsaw_to_netcdf/
+JIGSAW2NETCDF=$MPASTOOLS/grid_gen/triangle_jigsaw_to_netcdf/
 MESHCONVERTER=$MPASTOOLS/grid_gen/mesh_conversion_tools/MpasMeshConverter.x
 CELLCULLER=$MPASTOOLS/grid_gen/mesh_conversion_tools/MpasCellCuller.x
+VTKEXTRACTOR=$MPASTOOLS/python_scripts/paraview_vtk_field_extractor/paraview_vtk_field_extractor.py
 
 # First argument to this shell script is the name of the mesh
 MESHNAME=$1
@@ -31,32 +32,38 @@ else
 	 exit
 fi
 
-echo 'Build mesh using JIGSAW ...'
-CMD='try, run('"'$NAME.m'"'), catch, exit(1), end, exit(0);' # delete later
+echo
+echo 'Step 1. Build mesh using JIGSAW ...'
 echo $MATLAB "driver_jigsaw_to_mpas('$MESHNAME')"
+echo
 $MATLAB "driver_jigsaw_to_mpas('$MESHNAME')"
 echo 'done'
-
+echo
+echo 'Step 2. Convert triangles from jigsaw format to netcdf...'
 cd generated_meshes/$MESHNAME
-echo 'triangle_jigsaw_to_netcdf.py -s -m ${MESHNAME}-MESH.msh -o ${MESHNAME}_triangles.nc'
+echo "triangle_jigsaw_to_netcdf.py -s -m ${MESHNAME}-MESH.msh -o ${MESHNAME}_triangles.nc"
 $JIGSAW2NETCDF/triangle_jigsaw_to_netcdf.py -s -m ${MESHNAME}-MESH.msh -o ${MESHNAME}_triangles.nc
 echo 'done'
-
-echo "MpasMeshConverter.x ${MESHNAME}_triangles.nc ${MESHNAME}_all_cells.nc"
-$MESHCONVERTER ${MESHNAME}_triangles.nc ${MESHNAME}_all_cells.nc
-echo 'done'
-
+echo
+echo 'Step 3. Convert from triangles to MPAS mesh...'
+echo "MpasMeshConverter.x ${MESHNAME}_triangles.nc ${MESHNAME}_base_mesh.nc"
+$MESHCONVERTER ${MESHNAME}_triangles.nc ${MESHNAME}_base_mesh.nc
 echo 'Injecting bathymetry ...'
 ln -isf $JIGSAW/jigsaw/geo/topo.msh .
-echo "inject_bathymetry.py ${MESHNAME}_all_cells.nc"
-$JIGSAW2NETCDF/inject_bathymetry.py ${MESHNAME}_all_cells.nc
+echo "inject_bathymetry.py ${MESHNAME}_base_mesh.nc"
+$JIGSAW2NETCDF/inject_bathymetry.py ${MESHNAME}_base_mesh.nc
 echo 'done'
-
-echo 'Culling cells...'
-echo "MpasCellCuller.x ${MESHNAME}_all_cells.nc ${MESHNAME}_culled.nc"
-$CELLCULLER ${MESHNAME}_all_cells.nc ${MESHNAME}_culled.nc
+echo
+echo 'Step 4. (Optional) Create vtk file for visualization'
+echo "paraview_vtk_field_extractor.py ${MESHNAME}_base_mesh.nc ${MESHNAME}_vtk"
+$VTKEXTRACTOR --ignore_time -d maxEdges=0 -v allOnCells -f  ${MESHNAME}_base_mesh.nc -o ${MESHNAME}_vtk
+echo 'Injecting bathymetry ...'
+echo "inject_bathymetry.py ${MESHNAME}_culled.nc"
+$JIGSAW2NETCDF/inject_bathymetry.py ${MESHNAME}_culled.nc
 echo 'done'
-
+echo 'Step 5. (Optional) Cull land cells using topo.msh.  This is a quick cull, not the method for a production mesh.'
+echo "MpasCellCuller.x ${MESHNAME}_base_mesh.nc ${MESHNAME}_culled.nc"
+$CELLCULLER ${MESHNAME}_base_mesh.nc ${MESHNAME}_culled.nc
 echo 'Injecting bathymetry ...'
 echo "inject_bathymetry.py ${MESHNAME}_culled.nc"
 $JIGSAW2NETCDF/inject_bathymetry.py ${MESHNAME}_culled.nc
---
+++ grid_gen/jigsaw_to_MPAS/define_mesh/QU240.m
@@ -0,0 +1,3 @@
+function gridCellSizeGlobal = QU240(lon,lat)
+
+    gridCellSizeGlobal = 240*ones([length(lon) length(lat)]);
